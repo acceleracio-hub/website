a7becbe005567429408a1b9066f36b21cafe1131
acceleracio <acceleracio.info@gmail.com>
2024-08-16 17:30:16 +0400

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Header } from './components/header';
+import './styles.scss';
+
+function App() {
+  return (
+    <div className="container">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
